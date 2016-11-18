992607fad8e9c321ccbc35a3f5ac9095e6c76e30
ZebGirouard <timzebgir@gmail.com>
2016-11-18 14:24:41 -0700

    Catifying soln and starter code

--- solution-code/app/js/jquery.app.js
+++ solution-code/app/js/jquery.app.js
@@ -1,37 +1,39 @@
 // jQuery
 $(document).ready(function(){
-  $('form#new-doughnut').on('submit', sendOurDataViaAJAX)
-  getDoughnuts();
+  $('form#new-cat').on('submit', sendOurDataViaAJAX);
+  getCats();
 });
 
 function sendOurDataViaAJAX(e){
   e.preventDefault();
 
   // our API uses JSON, so we need to make a javascript object! There are a lot of ways to do this, this just a basic example.
-  var doughnut = {
-    style: $('form#new-doughnut select#doughnut-style').val(),
-    flavor: $('form#new-doughnut input#doughnut-flavor').val()
+  var cat = {
+    note: $('form#new-cat textarea#cat-note').val(),
+    name: $('form#new-cat input#cat-name').val()
   };
 
   // create a new AJAX request
-  $.post('https://api.doughnuts.ga/doughnuts', doughnut)
+  $.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(cat))
     .done(function(data){
-      addDoughnut(data);
+      addCat(JSON.parse(data));
     });
 
   // clear our input box!
-  $('form#new-doughnut input#doughnut-flavor').val(null)
+  $('form#new-cat input#cat-name').val(null);
 }
 
-function getDoughnuts(){
-  var doughnuts = $.get('https://api.doughnuts.ga/doughnuts')
+function getCats(){
+  var cats = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
     .done(function(data){
-      $.each(data, function(index, doughnut){
-        addDoughnut(doughnut);
+      console.log(data);
+      JSON.parse(data).forEach(function(cat){
+        console.log(cat);
+        addCat(cat);
       });
     });
 }
 
-function addDoughnut(doughnut) {
-  $("ul#doughnuts").prepend("<li>" + doughnut.flavor + " - <em>" + doughnut.style + "</em></li>")
+function addCat(cat) {
+  $("ul#cats").prepend("<li>" + cat.name + " - <em>" + cat.note + "</em></li>");
 }
